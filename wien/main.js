7c169d0f3f046a5b5b736e57df3c6fcfada85125
Fabian Igelsboeck <Fabian.Igelsboeck@student.uibk.ac.at>
2022-05-05 16:35:46 +0200

    wappen und Zonen hz

--- wien/main.js
+++ wien/main.js
@@ -92,3 +92,17 @@ async function loadLines(url) {
     L.geoJSON(geojson).addTo(overlay);
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Fußgängerzonen Wien
+async function loadZones(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup()
+    layerControl.addOverlay(overlay, "Fußgängerzonen Wien");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
