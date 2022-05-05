ff61bee86e4adeda2b5e0bf1d2b10baee6a0d3f0
Fabian Igelsboeck <Fabian.Igelsboeck@student.uibk.ac.at>
2022-05-05 16:26:10 +0200

    Haltestellen hz

--- wien/main.js
+++ wien/main.js
@@ -64,3 +64,17 @@ async function loadSites(url) {
     L.geoJSON(geojson).addTo(overlay);
 }
 loadSites(" https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Haltestellen Vienna Sightseeing
+async function loadStops(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup()
+    layerControl.addOverlay(overlay, "Vienna Sightseeing Haltestellen");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+}
+loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
