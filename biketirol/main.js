7b4b1242c2c1c6ccacb2076179cd8b6945a35605
Fabian Igelsboeck <Fabian.Igelsboeck@student.uibk.ac.at>
2022-06-09 16:04:30 +0200

    styling für marker und linie

--- biketirol/main.js
+++ biketirol/main.js
@@ -75,6 +75,16 @@ overlays.gpx.addTo(map);
 
 // GPX Track Layer implementieren
 let gpxTrack = new L.GPX("../data/10.gpx", {
-    async: true, 
+    async: true,
+    marker_options: {startIconUrl: 'icons/start.png',
+    endIconUrl: 'icons/finish.png',
+    shadowUrl: null,
+    iconSize: [32, 37],
+    iconAnchor: [16, 37],
+    },
+    polyline_options: {
+        color: "black",
+        dashArray: [2,4],
+    },
 }).addTo(overlays.gpx);
 
